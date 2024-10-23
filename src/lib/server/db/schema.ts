d639e056cfbb4326d3fd6447a99bc29ec1efe624
leif <leif@elliottnet.com>
2024-10-23 12:34:19 -0400

    sveltekit, svelte5, drizzle, nodejs, docker w/ docker-compose for concurrency, sqlite; DAMN this stack is MEMORY inefficient. My hope is it'll be fun to work on though.

---
+++ src/lib/server/db/schema.ts
@@ -0,0 +1,32 @@
+import { sqliteTable, text, integer, index } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
+
+export const users = sqliteTable(
+	'users',
+	{
+		id: text('id', { length: 36 })
+			.primaryKey()
+			.$defaultFn(() => crypto.randomUUID()),
+		payoutAddress: text('payout_address'),
+		createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`(unixepoch())`),
+		email: text('email').notNull(),
+		emailNotifications: integer('email_notifications', { mode: 'boolean' }).default(true)
+	},
+	(table) => {
+		return {
+			emailIdx: index('users_email_idx').on(table.email),
+			emailNotificationsIdx: index('users_email_notifications_idx').on(table.emailNotifications)
+		};
+	}
+);
+
+export const authOtps = sqliteTable('auth_otps', {
+	id: text('id', { length: 36 })
+		.primaryKey()
+		.$defaultFn(() => crypto.randomUUID()),
+	email: text('email').notNull(),
+	otp: text('otp').notNull(),
+	createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`(unixepoch())`),
+	/** expiration is always set 5 minutes after OTP creation. */
+	expiration: integer('expiration', { mode: 'timestamp' }).default(sql`(unixepoch() + (5 * 60))`)
+});
